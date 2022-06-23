4cf3e693974676217393521033ac0c9ad6063e40
Haleemah Korodo <haleemahkorodo@gmail.com>
2022-06-23 10:56:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var firstName='Haleemah';
+var middleName='Abosede';
+var lastName='Korodo';
+var height="5'5ft";
+var country='Nigeria';
+
+name=firstName+" "+ middleName+" "+lastName;
+
+document.write('Name:'+ ' '+ name+ '</br>');
+document.write('Height:'+ ' '+ height+ '</br>');
+document.write('Country:'+ ' '+ country + '</br>');
